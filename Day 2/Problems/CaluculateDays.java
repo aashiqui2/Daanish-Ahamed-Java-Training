842fb001a01ddd692e3d5ae1e90ef8b2701569cd
Aashiqui <ashikmail2747@gmail.com>
2025-02-05 22:09:49 +0530

    day 3 completed

--- Day 2/Problems/CaluculateDays.java
+++ Day 2/Problems/CaluculateDays.java
@@ -1,3 +1,4 @@
+package Problems;
 import java.util.Scanner;
 public class CaluculateDays {
     public static void main(String[] args) {
---
+++ Day 3/Forloop.java
@@ -0,0 +1,28 @@
+public class Forloop {
+    public static void main(String[] args) {
+        // for(int i=0;i<=5;i++)
+        // {
+        // System.out.print(i+" ");
+        // }
+
+        // int i = 0;
+        // for (; i < 5; i++) {
+        //     System.out.print(i + " ");
+        // }
+        // System.out.println("the value of i is " +i);
+
+       int i=0;
+       for(;i<5;)
+       {
+        System.out.print(i+" ");//0 1 2 3 4
+        i++;//1 2 3 4 5
+       }
+       System.out.println(" the value of i is "+i);
+
+
+
+
+
+
+    }
+}
