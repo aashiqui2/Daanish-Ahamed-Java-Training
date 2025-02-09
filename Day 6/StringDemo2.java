ccd6de03efe892b264b9fe369a0ba0e00553defa
Aashiqui <ashikmail2747@gmail.com>
2025-02-09 19:29:28 +0530

    day 6 added

---
+++ Day 6/StringDemo2.java
@@ -0,0 +1,24 @@
+public class StringDemo2 {
+    public static void main(String[] args) {
+        // String str="hello";
+
+        String str=new String("hello");
+        String str1=new String("hello");
+
+        // if(str==str1)
+        // {
+        //     System.out.println("same");
+        // }
+        // else{
+        //     System.out.println("not same");
+        // }
+
+        if(str.equals(str1))
+        {
+            System.out.println("same");
+        }
+        else{
+            System.out.println("not same");
+        }
+    }
+}
