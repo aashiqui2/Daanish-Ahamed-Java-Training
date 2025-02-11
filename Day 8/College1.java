0464c96c8063515f741e9628e44bd38724ea6581
muhmdpp <connect.mhdpp@gmail.com>
2025-02-11 19:42:14 +0530

    day 8 updation

---
+++ Day 8/College1.java
@@ -0,0 +1,19 @@
+package tamilnadu.chennai;
+import tamilnadu.chennai.College;
+public class College1 {
+
+    public static void main(String[] args) {
+        College1 clg1=new College1();
+        clg1.display();
+
+        College clg=new College();
+        clg.studying();
+        clg.recruit();
+        clg.write();
+        clg.semesterpaper();
+    }
+    void display()
+    {
+        System.out.println("Welcome to college 1");
+    }
+}
